cefcd68c302d85db820bb02e2ca824cde186d76a
mseryn <melanie.e.cornelius@gmail.com>
2016-12-30 09:25:30 -0600

    initial stub table creation

---
+++ src/database/table_creation.sql
@@ -0,0 +1,71 @@
+/*
+GroceryDirect: Database Creation
+
+Author: Melanie Cornelius, mseryn
+Written for CS 425-02 Fall 2016 Project
+*/
+
+/*
+ * Primary Tables
+*/
+
+create table persons 
+(
+)
+
+create table orders
+(
+)
+
+create table products
+(
+)
+
+create table warehouses
+(
+)
+
+create table addresses
+(
+)
+
+create table credit_cards
+(
+)
+
+/*
+ * Enumeration Tables
+*/
+
+create table person_types
+(
+)
+
+create table product_types
+(
+)
+
+create table address_types
+(
+)
+
+create table completion_status
+(
+)
+
+create table state_codes
+(
+    /* Note: table exists for ease of code verification */
+)
+
+/*
+ * Link Tables
+*/
+
+create table order_to_product
+(
+)
+
+create tabe warehouse_to_product
+(
+)
