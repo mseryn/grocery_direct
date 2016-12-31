e4b0022e5a21c62e18f2a272017d830d17adba53
mseryn <melanie.e.cornelius@gmail.com>
2016-12-31 07:22:35 -0600

    adding stub files for unit tests of person types

---
+++ src/tests/unit_tests/staff_tests.py
@@ -0,0 +1,27 @@
+#***
+#*  GroceryDirect - Staff Member Unit Tests
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#***
+
+#***
+#*  Actions to take as staff member:
+#*      - Log in
+#*      - Log out
+#*      - View all products 
+#*      - Add products
+#*      - Delete products
+#*      - Modify ALL aspects of products (safely? not requirement)
+#*          -- price, image, description, nutrition facts, alcohol content
+#*      - View all warehouses
+#*      - Add products to warehouse
+#*      - Remove products from warehouse
+#*      - Account administration:
+#*          -- change name
+#*          -- change address
+#*          -- change username
+#*          -- change password
+#***
+
+
---
+++ src/tests/unit_tests/supplier_tests.py
@@ -0,0 +1,23 @@
+#***
+#*  GroceryDirect - Supplier Unit Tests
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#***
+
+#***
+#*  Actions to take as supplier:
+#*      - Log in
+#*      - Log out
+#*      - View all products in supplier's stock
+#*      - Add products to supplier's stock
+#*      - Delete products from supplier's stock
+#*      - Modify ALL aspects of products (safely? not requirement)
+#*          -- price, image, description, nutrition facts, alcohol content
+#*      - Account administration:
+#*          -- change address
+#*          -- change username
+#*          -- change password
+#***
+
+
