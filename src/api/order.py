f058b990cc4705d0416687b7956ee82b73154a9d
mseryn <melanie.e.cornelius@gmail.com>
2017-01-04 12:45:46 -0600

    starting to implement order

---
+++ src/api/order.py
@@ -0,0 +1,32 @@
+#***
+#*  GroceryDirect - Product Tests
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#*
+#*  Order Description:
+#*  - has:
+#*      -- id
+#*      -- product list
+#*      -- shipping address
+#*      -- status
+#*      -- placement date -- TODO, how implement?
+#*      -- customer (owner) -- TODO, how implement? Very vital.
+#*  - get:
+#*      -- product list
+#*      -- total cost of products
+#*      -- shipping address
+#*      -- shipping state (for ease) - TODO
+#*      -- order placement date
+#*      -- order status
+#*      -- customer ID
+#*  - modify:
+#*      -- product quantity -- TODO, vital
+#*      -- shipping address
+#*      -- order placement date -- TODO, how implement?
+#*      -- order status
+#*  - add/remove:
+#*      -- products from list
+#***
+
+ 
