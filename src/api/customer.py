b58d43f8e9e84fb28600aee5fad074e31dd53e5c
mseryn <melanie.e.cornelius@gmail.com>
2017-01-06 14:13:50 -0600

    preparing files for move to Python 2 and Oracle Linux virt:

---
+++ src/api/customer.py
@@ -0,0 +1,12 @@
+#***
+#*  GroceryDirect - Customer API
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#*
+#***
+
+class Customer():
+    def __init__(self):
+        pass
+
