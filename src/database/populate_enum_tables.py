d23276d19c16532aaee91ee993c5e811a15fd0c6
mseryn <melanie.e.cornelius@gmail.com>
2017-01-09 18:24:58 -0500

    adding enum table populator -- Python script

---
+++ src/database/populate_enum_tables.py
@@ -0,0 +1,28 @@
+#***
+#*  GroceryDirect - Script to Populate Enum Tables
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#***
+
+# People Types
+people_types = ["customer", "staff", "warehouse", "supplier", "administrator"]
+
+
+# Product Types
+product_types = ["non-food", "food", "non-alcoholic beverage", "alcoholic beverage"]
+
+# Address Types
+address_types = ["delivery", "shipping", "warehouse"]
+
+# Order Statuses
+order_statuses = ["pending", "shipping", "delivered", "canceled"]
+
+# State Codes
+state_codes = []
+file_descriptor = open(state_code_file_with_path, 'r')
+for line in
+
+# Card Types
+
+card_types = ["Visa", "Mastercard", "AmericanExpress"]
