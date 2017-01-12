8d007bd3329f7006032249cf5e3300a8a6853779
mseryn <melanie.e.cornelius@gmail.com>
2017-01-12 01:28:56 -0500

    adding address tests

---
+++ src/tests/unit_tests/address_tests.py
@@ -0,0 +1,77 @@
+#***
+#*  GroceryDirect - Address Tests
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#*
+#*  Addresses:
+#*  have:
+#*      -- street
+#*      -- apt (optional)
+#*      -- city
+#*      -- state
+#*      -- zip code
+#*      -- type
+#*  get:
+#*      -- street
+#*      -- apt (if exists)
+#*      -- city
+#*      -- state
+#*      -- zip code
+#*      -- full address string?
+#*      -- type
+#*  modify:
+#*      -- street
+#*      -- apt
+#*      -- city
+#*      -- state
+#*      -- zip code
+#*      -- type
+#***
+
+#**************************************************************************************************
+#**  ADDRESS: STREET
+#**************************************************************************************************
+
+def test_address_get_street():
+def test_address_modify_street():
+
+#**************************************************************************************************
+#**  ADDRESS: APARTPEMNT NUMBER
+#**************************************************************************************************
+
+def test_address_get_apt():
+def test_address_get_apt_null():
+def test_address_modify_apt():
+
+#**************************************************************************************************
+#**  ADDRESS: APARTPEMNT NUMBER
+#**************************************************************************************************
+
+def test_address_get_city():
+def test_address_modify_city():
+
+#**************************************************************************************************
+#**  ADDRESS: ZIPCODE
+#**************************************************************************************************
+
+def test_address_get_zip():
+def test_address_modify_zip():
+def test_address_modify_zip_invalid_type():
+def test_address_modify_zip_invalid_number():
+
+#**************************************************************************************************
+#**  ADDRESS: STATE
+#**************************************************************************************************
+
+def test_address_get_state():
+def test_address_modify_state():
+def test_address_modify_state_invalid():
+
+#**************************************************************************************************
+#**  ADDRESS: TYPE
+#**************************************************************************************************
+
+def test_address_get_type():
+def test_address_modify_type():
+def test_address_modify_type_invalid():
