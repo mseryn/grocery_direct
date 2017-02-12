418c5cb7b299a2df3eebf2e48eb8242bb77feff8
mseryn <melanie.e.cornelius@gmail.com>
2017-02-12 13:49:07 -0500

    Adding credit card tests

---
+++ src/api/credit_card.py
@@ -0,0 +1,292 @@
+#***
+#*  GroceryDirect - Credit Card Class
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#*  
+#*  Warehouse Description:
+#*  - has:
+#*      -- id
+#*      -- address (id)
+#*      -- card number
+#*      -- security code
+#*      -- expiration date
+#*      -- type 
+#*      -- person ID
+#*  - get:
+#*      -- address (id)
+#*      -- card number
+#*      -- security code
+#*      -- expiration date
+#*      -- type 
+#*      -- person ID
+#*  - modify:
+#*      -- address (id)
+#*      -- card number
+#*      -- security code
+#*      -- expiration date
+#*      -- type 
+#***
+
+import address
+import database
+import person
+
+
+class CreditCard():
+
+    def __init__(self, given_id):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        # Ensuring given ID is int
+        if isinstance(given_id, int):
+            # Ensuring warehouse ID is in credit card table
+            cursor.execute("select id from credit_cards where id = :card_id", card_id = given_id)
+            if cursor.fetchone():
+                self._id = given_id
+            else:
+                raise ValueError("Given ID not in credit cards table, id: %i" %given_id)
+        else:
+            raise ValueError("Given ID not an int, id: %s" %str(given_id))
+        database.disconnect(db)
+
+    @staticmethod
+    def new_creditcard(person_reference, card_number, security_code, expiration_month, \
+        expiration_year, card_type, address_reference):
+        # Adding new warehouse to database, and returning reference to that warehouse
+        db = database.connect()
+        cursor = database.get_cursor(db)
+
+        # Ensuring address is in table
+        cursor.execute("select id from addresses where id = :input_id", \
+            input_id = address_reference.get_id())
+        address_id = cursor.fetchone()
+        if address_id:
+            address_id = address_id[0]
+        else:
+            raise ValueError("Given address is not in address table")
+
+        # Ensuring person is in table
+        cursor.execute("select id from persons where id = :input_id", \
+            input_id = person_reference.get_id())
+        person_id = cursor.fetchone()
+        if person_id:
+            person_id = person_id[0]
+        else:
+            raise ValueError("Given person is not in person table")
+
+        # type-checking numeric inputs
+        if not isinstance(card_number, int):
+            raise ValueError("Card number must be integer value")
+        if not isinstance(security_code, int):
+            raise ValueError("Security code must be integer value")
+        if not isinstance(expiration_month, int):
+            raise ValueError("Expiration month must be integer value")
+        if not isinstance(expiration_year, int):
+            raise ValueError("Expiration year must be integer value")
+
+        # value checking numeric inputs
+        if not len(card_number) == 16:
+            raise ValueError("Card number must be int of 16 digits")
+        if not len(security_code) == 3:
+            raise ValueError("Security code must be into of 3 digits")
+        if expiration_month > 12 or expiration_month < 1:
+            raise ValueError("Expiration month must be between 1 and 12")
+        if not len(expiration_year) == 4:
+            raise ValueError("Expiration year must be 4 digit integer")
+
+
+        # TODO: FINISH FROM HERE
+
+        if isinstance(capacity, (int, float)):
+            returned_id = cursor.var(database.cx_Oracle.NUMBER)
+            capacity = int(capacity)
+            cursor.execute("insert into warehouses \
+                            (capacity, address_id) values (:input_capacity, :input_address) \
+                            returning id into :new_warehouse_id", \
+                            input_capacity = capacity, input_address = address_id, \
+                            new_warehouse_id = returned_id)
+            database.commit(db)
+
+        returned_id = int(returned_id.getvalue())
+
+        database.disconnect(db)
+        return Warehouse(returned_id)
+
+    # Get Methods
+
+    def get_id(self):
+        return self._id
+
+    def get_capacity(self):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        cursor.execute("select capacity from warehouses where id = :warehouse_id", \
+                        warehouse_id = self.get_id())
+        cap = cursor.fetchone()[0]
+        database.disconnect(db)
+        return cap
+
+    def get_stock(self):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        stock = []
+        cursor.execute("select product_id \
+                        from warehouses join warehouse_to_product \
+                        on warehouses.id = warehouse_to_product.warehouse_id \
+                        where warehouses.id = :warehouse_id", warehouse_id = self.get_id())
+        product_list = cursor.fetchall()
+        if product_list:
+            for product_id in product_list:
+                stock.append(product.Product(int(product_id[0])))
+        database.disconnect(db)
+        return stock
+
+    def get_product_quantity(self, product):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        cursor.execute("select quantity \
+                        from warehouses join warehouse_to_product \
+                        on warehouses.id = warehouse_to_product.warehouse_id \
+                        where warehouses.id = :warehouse_id \
+                        and warehouse_to_product.product_id = :input_product", \
+                        warehouse_id = self.get_id(), input_product = product.get_id())
+        product_quantity = cursor.fetchone()
+        if product_quantity:
+            return product_quantity[0]
+        else:
+            return 0
+
+    def get_remaining_capacity(self):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        total_used_space = 0
+        for product in self.get_stock():
+            total_used_space += product.get_size() * self.get_product_quantity(product)
+        remaining_cap = self.get_capacity() - total_used_space
+        database.disconnect(db)
+        return remaining_cap
+
+    def get_address(self):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        cursor.execute("select address_id from warehouses where id = :warehouse_id", \
+                        warehouse_id = self.get_id())
+        returned_id = cursor.fetchone()
+        if returned_id:
+            returned_id = returned_id[0]
+            database.disconnect(db)
+            if isinstance(returned_id, int):
+                warehouse_address = address.Address(returned_id)
+                return warehouse_address
+        else:
+            database.disconnect(db)
+            return None
+
+    # Modification Methods
+
+    def modify_capacity(self, new_capacity):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        if isinstance(new_capacity, (int, float)):
+            if new_capacity >=0:
+                if new_capacity >= (self.get_capacity() - self.get_remaining_capacity()):
+                    cursor.execute("update warehouses set capacity = :input_capacity \
+                                    where id = :warehouse_id", input_capacity = int(new_capacity), \
+                                    warehouse_id = self.get_id())
+                    database.commit(db)
+                else:
+                    raise ValueError("New capacity must be >= remaining capacity")
+            else:
+                raise ValueError("Capacity must be >=0")
+        else:
+            raise ValueError("New capacity must be integer \nInput capacity: %s" %(str(new_capacity)))
+        database.disconnect(db)
+
+    def add_product(self, new_product):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        product_id = new_product.get_id()
+        cursor.execute("select quantity from warehouse_to_product \
+                        where product_id = :input_pid and warehouse_id = :input_wid", \
+                        input_pid = product_id, input_wid = self.get_id())
+        current_quantity = cursor.fetchone()
+        if current_quantity:
+            # The item is already in this table in the DB, just incriment quantity
+            incrimented_quantity = int(current_quantity[0]) + 1
+            cursor.execute("update warehouse_to_product set quantity = :input_quantity \
+                            where product_id = :input_pid and warehouse_id = :input_wid", \
+                            input_quantity = incrimented_quantity, input_pid = product_id, \
+                            input_wid = self.get_id())
+            database.commit(db)
+            database.disconnect(db)
+        else:
+            # The item is not yet in the warehouse's stock, so add it to the table
+            cursor.execute("insert into warehouse_to_product (product_id, warehouse_id, quantity) \
+                            values (:input_pid, :input_wid, :input_quantity)", \
+                            input_pid = product_id, input_wid = self.get_id(), input_quantity = 1)
+            database.commit(db)
+            database.disconnect(db)
+
+    def remove_product(self, product):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        product_id = product.get_id()
+        cursor.execute("select quantity from warehouse_to_product \
+                        where product_id = :input_pid and warehouse_id = :input_wid", \
+                        input_pid = product_id, input_wid = self.get_id())
+        current_quantity = cursor.fetchone()
+
+        if current_quantity:
+            # The item is already in this table in the DB, just decriment quantity
+            decrimented_quantity = int(current_quantity[0]) - 1
+            if decrimented_quantity > 0:
+                # Removing one will not remove all instances of that product
+                cursor.execute("update warehouse_to_product set quantity = :input_quantity \
+                                where product_id = :input_pid and warehouse_id = :input_wid", \
+                                input_quantity = decrimented_quantity, input_pid = product_id, \
+                                input_wid = self.get_id())
+                database.commit(db)
+            else:
+                # Remove the line from the DB if product has quantity of zero
+                cursor.execute("delete from warehouse_to_product \
+                                where product_id = :input_pid and warehouse_id = :input_wid", \
+                                input_pid = product_id, input_wid = self.get_id())
+                database.commit(db)
+        else:
+            # The item is not yet in the warehouse's stock, so do nothing
+            pass
+        database.disconnect(db)
+
+    def modify_quantity(self, product, new_quantity):
+        db = database.connect()
+        cursor = database.get_cursor(db)
+        if isinstance(new_quantity, int) and new_quantity >= 0:
+            product_id = product.get_id()
+            cursor.execute("select quantity from warehouse_to_product \
+                            where product_id = :input_pid and warehouse_id = :input_wid", \
+                            input_pid = product_id, input_wid = self.get_id())
+            current_quantity = cursor.fetchone()
+
+            if current_quantity:
+                # Ensuring product is in warehouse stock
+                current_quantity = int(current_quantity[0])
+                if current_quantity == new_quantity:
+                    # Do nothing if quantity doesn't change
+                    pass
+                elif new_quantity == 0: 
+                    # Remove line in DB is new quantity is zero
+                    cursor.execute("delete from warehouse_to_product \
+                                    where product_id = :input_pid and warehouse_id = :input_wid", \
+                                    input_pid = product_id, input_wid = self.get_id())
+                    database.commit(db)
+                else:
+                    # Otherwise just update the quantity
+                    cursor.execute("update warehouse_to_product set quantity = :input_quantity \
+                                    where product_id = :input_pid and warehouse_id = :input_wid", \
+                                    input_quantity = new_quantity, input_pid = product_id, \
+                                    input_wid = self.get_id())
+                    database.commit(db)
+        else:
+            raise ValueError("new quantity must be positive integer value")
+        database.disconnect(db)
---
+++ src/tests/unit_tests/credit_card_tests.py
@@ -0,0 +1,144 @@
+#***
+#*  GroceryDirect - Credit Card Unit Tests
+#*  
+#*  Author: Melanie Cornelius, mseryn
+#*  Written for CS 425-02 Fall 2016 Final Project
+#*
+#***
+
+import address
+import credit_card
+import person
+
+import datetime
+
+#************************************************************************************************#
+#** DEFAULTS FOR TESTS
+#************************************************************************************************#
+
+# Default values for TEST_CARD
+CARD_NO     = 1234123456785678 
+SEC_CODE    = 321
+MONTH       = 8
+YEAR        = 2020
+TYPE        = "visa"
+
+# Default strings for test bill_address
+BILL_STREET         = "654 Billing St."
+BILL_CITY           = "Billing City"
+BILL_STATE_CODE     = "MI"
+BILL_ZIP_CODE       = 98765
+BILL_TYPE_STRING    = "billing"
+
+# Default strings for test person
+FNAME       = "FirstName"
+LNAME       = "LastName"
+MIDDLE_INIT = "M"
+USERNAME    = "testaccount"
+PASSWORD    = "testpassword"
+
+# Test objects (avoid cluttering db)
+BILL_ADDRESS = address.Address.new_address(BILL_STREET, BILL_CITY, BILL_STATE_CODE, \
+                    BILL_ZIP_CODE, BILL_TYPE_STRING)
+TEST_PERSON = person.check_credentials(USERNAME, PASSWORD)
+if not TEST_PERSON:
+    # Since persons are unique, have to be careful when making new test person - might already
+    # exist
+    TEST_PERSON  = person.Person.new_person(USERNAME, PASSWORD, FNAME, LNAME, "customer", \
+                        middle_initial = MIDDLE_INIT)
+TEST_CARD = credit_card.CreditCard.new_credit_card(TEST_PERSON, CARD_NO, SEC_CODE, MONTH, \
+                    YEAR, TYPE, BILL_ADDRESS)
+
+#**************************************************************************************************
+#**  PERSON
+#**************************************************************************************************
+
+def test_get_person():
+    assert(TEST_CARD.get_person().get_id() == TEST_PERSON.get_id()), \
+        "get_person did not return person reference for credit card"
+
+#**************************************************************************************************
+#**  CARD NUMBER
+#**************************************************************************************************
+
+def test_get_card_no():
+    assert(TEST_CARD.get_card_number() == CARD_NO % 1000), \
+        "get_card_number did not return card number for credit card"
+
+def test_modify_card_no():
+    new_number = (9876987698769876) 
+    TEST_CARD.modify_card_number(new_number) 
+    stored_card_no = TEST_CARD.get_card_number()
+    TEST_CARD.modify_card_number(CARD_NO)
+    assert(stored_card_no == new_number % 1000), \
+        "modify_card_number did not modify and return card number for credit card"
+
+#**************************************************************************************************
+#**  SECURITY CODE NUMBER
+#**************************************************************************************************
+
+def test_check_security_code():
+    assert(TEST_CARD.check_security_code(SEC_CODE) == True), \
+        "check_security_code did not return True for proper input in credit card"
+
+def test_check_security_code_false():
+    assert(TEST_CARD.check_security_code(000) == False), \
+        "check_security_code did not return False for improper input in credit card"
+
+def test_modify_security_code():
+    new_code = 888
+    TEST_CARD.modify_security_code(new_code)
+    stored_boolean = TEST_CARD.check_security_code(new_code)
+    TEST_CARD.modify_security_code(SEC_CODE)
+    assert(stored_boolean == True), \
+        "modify_security_code did not modify and return security code in credit card"
+
+#**************************************************************************************************
+#**  ADDRESS
+#**************************************************************************************************
+
+def test_get_address():
+    assert(TEST_CARD.get_address().get_id() == BILL_ADDRESS.get_id()), \
+        "get address failed to return correct address id in credit card"
+
+def test_modify_address():
+    pass
+
+#**************************************************************************************************
+#**  EXPIRATION DATE
+#**************************************************************************************************
+
+def test_get_expiration_date():
+    returned_date = TEST_CARD.get_expiration_date()
+    month = returned_date.month
+    year = returned_date.year
+    assert(month == MONTH and year == YEAR), \
+        "get expiration date failed to return correct expiration date in credit card"
+
+def test_modify_expiration_date():
+    new_month = 1
+    new_year = 2022
+    TEST_CARD.modify_expiration_date(new_month, new_year)
+    returned_date = TEST_CARD.get_expiration_date()
+    TEST_CARD.modify_expiration_date(MONTH, YEAR)
+    month = returned_date.month
+    year = returned_date.year
+    assert(month == new_month and year == new_year), \
+        "modify expiration date failed to modify expiration date in credit card"
+
+
+#**************************************************************************************************
+#**  TYPE
+#**************************************************************************************************
+
+def test_get_type():
+    assert(TEST_CARD.get_card_type() == TYPE), \
+        "get card type failed to return credit card type"
+
+def test_modify_type():
+    new_type = "mastercard"
+    TEST_CARD.modify_type(new_type)
+    stored_type = TEST_CARD.get_card_type()
+    TEST_CARD.modify_type(TYPE)
+    assert(stored_type == new_type), \
+        "modify card type failed to return/modfiy credit card type."
